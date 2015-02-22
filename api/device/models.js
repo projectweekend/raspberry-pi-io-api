195f512d29145cb6142bd8732aa3921cf02cd5fa
Brian Hines <brian@projectweekend.net>
2015-02-22 00:12:34 -0600

    Device mongoose models

---
+++ api/device/models.js
@@ -0,0 +1,51 @@
+var mongoose = require( "mongoose" );
+
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+
+var GPIOConfigSchema = Schema( {
+    pin: {
+        type: Number,
+        required: true
+    },
+    mode: {
+        type: String,
+        required: true
+    },
+    initial: {
+        type: String,
+        default: "LOW"
+    },
+    resistor: String,
+    pinEvent: String,
+    eventHandler: String,
+    bounce: Number
+} );
+
+
+var DeviceSchema = Schema ( {
+    id: ObjectId,
+    key: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    rabbitURL: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userEmail: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    gpioConfig: [ GPIOConfigSchema ]
+} );
+
+DeviceSchema.index( { key: 1, userEmail: 1 }, { unique: true } );
+
+
+exports.Device = mongoose.model( "Device", DeviceSchema );
