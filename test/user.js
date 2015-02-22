fb1594b4300dba2810086993117ec2f8b31f3b83
Brian Hines <brian@projectweekend.net>
2015-02-22 00:29:16 -0600

    More testing

--- test/user.js
+++ test/user.js
@@ -6,18 +6,24 @@ var supertest = require( "supertest" );
 var api = supertest( app );
 
 
-describe( "Register a new user with valid data", function () {
+var validEmail = "test@test.com";
+var validPassword = "SuperSecret";
 
-    var email = "test@test.com";
-    var password = "SuperSecret";
+var invalidEmail = "not an email";
+var invalidPassword = "short";
+
+var invalidEmailMessage = "Not a valid email address";
+
+
+describe( "Register a new user with valid data", function () {
 
     it( "responds with 201 and data", function ( done ) {
 
         api.post( "/register" )
             .set( "Content-Type", "application/json" )
             .send( {
-                email: email,
-                password: password
+                email: validEmail,
+                password: validPassword
             } )
             .expect( 201 )
             .end( function ( err, res ) {
@@ -27,6 +33,35 @@ describe( "Register a new user with valid data", function () {
                 }
 
                 expect( res.body ).to.have.a.property( "token" );
+
+                done();
+
+            } );
+
+    } );
+
+} );
+
+
+describe( "Register a new user with invalid email", function () {
+
+    it( "responds with 400 and a message", function ( done ) {
+
+        api.post( "/register" )
+            .set( "Content-Type", "application/json" )
+            .send( {
+                email: invalidEmail,
+                password: validPassword
+            } )
+            .expect( 400 )
+            .end( function ( err, res ) {
+
+                if ( err ) {
+                    return done( err );
+                }
+
+                expect( res.body ).to.be.an( "array" ).with.length.equal( 1 );
+                expect( res.body[ 0 ] ).to.have.a.property( "msg", invalidEmailMessage );
 
                 done();
 
