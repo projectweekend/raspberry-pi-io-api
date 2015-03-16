8045894dc894ae1cc8982f16d44f1df525bbfe8f
Brian Hines <brian@projectweekend.net>
2015-03-15 19:11:25 -0500

    Put the router back

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,29 @@
 var express = require( "express" );
 var router = express.Router();
-
 var user = require( "../api/user/handlers" );
 var device = require( "../api/device/handlers" );
 var raspberrypi = require( "../api/raspberrypi/handlers" );
 
 
-module.exports = function ( messageBroker ) {
-    router.post( "/register", user.register( messageBroker ) );
-    router.post( "/authenticate", user.authenticate );
+router.post( "/register", user.register );
+router.post( "/authenticate", user.authenticate );
 
-    router.get( "/user", user.getDetail );
-    router.delete( "/user", user.unRegister );
+router.get( "/user", user.getDetail );
+router.delete( "/user", user.unRegister );
 
-    router.post( "/user/key", user.generateKey );
+router.post( "/user/key", user.generateKey );
 
-    router.post( "/user/device", device.register );
-    router.get( "/user/device", device.list );
-    router.get( "/user/device/:deviceId", device.detail );
-    router.delete( "/user/device/:deviceId", device.remove );
+router.post( "/user/device", device.register );
+router.get( "/user/device", device.list );
+router.get( "/user/device/:deviceId", device.detail );
+router.delete( "/user/device/:deviceId", device.remove );
 
-    router.post( "/user/device/:deviceId/pin", device.addPin );
-    router.get( "/user/device/:deviceId/pin", device.listPins );
-    router.get( "/user/device/:deviceId/pin/:pinId", device.detailPin );
-    router.delete( "/user/device/:deviceId/pin/:pinId", device.removePin );
+router.post( "/user/device/:deviceId/pin", device.addPin );
+router.get( "/user/device/:deviceId/pin", device.listPins );
+router.get( "/user/device/:deviceId/pin/:pinId", device.detailPin );
+router.delete( "/user/device/:deviceId/pin/:pinId", device.removePin );
 
-    router.get( "/pin-config", raspberrypi.pinConfig );
+router.get( "/pin-config", raspberrypi.pinConfig );
 
-    return router;
-};
+
+module.exports = router;
