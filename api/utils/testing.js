bd9cddcafc472ff1854c82715f59341757d8dea3
Brian Hines <brian@projectweekend.net>
2015-03-17 05:42:47 -0500

    mock rabbit client

---
+++ api/utils/testing.js
@@ -0,0 +1,19 @@
+var sinon = require( "sinon" );
+var amqp = require( "./amqp" );
+
+
+exports.mockRabbitClient = function ( done ) {
+
+    var fakeClient = function ( rabbitUrl ) {
+
+    };
+
+    fakeClient.prototype.send = function( routingKey, message ) {
+
+    };
+
+    sinon.stub( amqp, "RabbitClient" ).yields( null, fakeClient );
+
+    return done();
+
+};
